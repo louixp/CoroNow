3943bf7366bbded538087f96f50381102a9152de
Louis Qin <qyl0509@icloud.com>
2020-03-24 14:13:01 -0700

    add keywords

--- twitter/scrape.py
+++ twitter/scrape.py
@@ -1,6 +1,11 @@
 import twint
 import datetime
 
+# keywords provided by https://arxiv.org/abs/2003.07372
+keywords = ["Coronavirus", "Koronavirus", "Corona", "CDC", "Wuhancoronavirus", "Wuhanlockdown", "Ncov", 
+            "Wuhan", "N95", "Kungflu", "Epidemic", "Outbreak", "Sinophobia", "China", "Covid-19 ", 
+            "Corona virus", "Covid", "Covid19", "Sars-cov-2", "COVID–19", "COVD", "Pandemic"]
+
 def get_tweets(keyword, limit):
     c = twint.Config()
     c.Search = keyword
