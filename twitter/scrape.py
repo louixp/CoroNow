fc34a2cc596a28c469a2976006697800f3d4c963
Louis Qin <qyl0509@icloud.com>
2020-03-24 14:30:08 -0700

    count hashtag frequency

--- twitter/scrape.py
+++ twitter/scrape.py
@@ -1,10 +1,11 @@
 import twint
 import datetime
+from collections import Counter
 
 # keywords provided by https://arxiv.org/abs/2003.07372
-keywords = ["Coronavirus", "Koronavirus", "Corona", "CDC", "Wuhancoronavirus", "Wuhanlockdown", "Ncov", 
+keywords = {"Coronavirus", "Koronavirus", "Corona", "CDC", "Wuhancoronavirus", "Wuhanlockdown", "Ncov", 
             "Wuhan", "N95", "Kungflu", "Epidemic", "Outbreak", "Sinophobia", "China", "Covid-19 ", 
-            "Corona virus", "Covid", "Covid19", "Sars-cov-2", "COVID–19", "COVD", "Pandemic"]
+            "Corona virus", "Covid", "Covid19", "Sars-cov-2", "COVID–19", "COVD", "Pandemic"}
 
 def get_tweets(keyword, limit):
     c = twint.Config()
@@ -15,6 +16,15 @@ def get_tweets(keyword, limit):
     c.Store_json = True
     c.Output = f"scraped_tweets_{datetime.date.today()}_{datetime.datetime.now().time()}.json"
     twint.run.Search(c)
+
+def clean_tweet(tweet):
+    pass
+
+def word_freq():
+    pass
+    
+def hashtag_freq():
+    return Counter([h for t in twint.output.tweets_list for h in t.hashtags])
 
 if __name__ == "__main__":
     get_tweets("covid-19", 100)
