b869bcfc892911b6a7ddc721c731fef98af97c71
zhao19991111 <49088390+zhao19991111@users.noreply.github.com>
2020-03-25 02:35:34 -0700

    firebase empowered

---
+++ twitter/config.py
@@ -0,0 +1,10 @@
+firebaseConfig = {
+    "apiKey": "AIzaSyD0e6NAyxUI5Z8rs1j2ahwCwpG2HptFg-o",
+    "authDomain": "coronow-d737d.firebaseapp.com",
+    "databaseURL": "https://coronow-d737d.firebaseio.com",
+    "projectId": "coronow-d737d",
+    "storageBucket": "coronow-d737d.appspot.com",
+    "messagingSenderId": "787414217798",
+    "appId": "1:787414217798:web:5a013427d62e4425da4647",
+    "measurementId": "G-X4NZ3H0HDS"
+}
---
+++ twitter/upload.py
@@ -0,0 +1,45 @@
+from firebase import firebaseAPI
+from config import firebaseConfig
+import glob
+import json
+import pprint
+
+def upload_dict(dictObj, api):
+    for key in dictObj.keys():
+        print(key)
+        for time_arr in dictObj[key]:
+            for time in time_arr.keys():
+                print("--"+time)
+                for keyword_arr in time_arr[time]:
+                    for keyword in keyword_arr.keys():
+                        print("----"+keyword)
+                        for tweet in keyword_arr[keyword]:
+                            print("-------"+str(tweet["id"]))
+                            api.store_data(key, tweet, [str(time), str(keyword), str(tweet["id"])])
+                            
+
+
+def main():
+    data_list = []
+    firebase = firebaseAPI(firebaseConfig)
+    for dir in glob.glob('./[0-9]*-[0-9]*-[0-9]*-[0-9]*'):
+        date = dir.split('\\')[1]
+        time_list = []
+        for jsonfile in glob.glob("./{0}/tweets_*".format(dir)):
+            keyword = jsonfile.split('\\')[2].replace(".json","").replace("tweets_", "")
+            tweet_list = []
+            db = open(jsonfile, "r", encoding = 'utf-8')
+            print("Database opened for reading")
+            for tweet_str in db:
+                tweet = json.loads(tweet_str)
+                tweet_list.append(tweet)
+            tweet_col = {keyword: tweet_list}
+            time_list.append(tweet_col)
+        time_col = {date: time_list}
+        data_list.append(time_col)
+    data_col = {"tweets" : data_list}
+    upload_dict(data_col, firebase)
+        
+
+main()
+        
