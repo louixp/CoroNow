516aa388ca852c0b665a2456e8ed84f34e1eb8b0
Johnny Wan <wonjohnny@outlook.com>
2020-03-25 11:38:05 -0700

    add a dummy wordcloud api

--- backend/server.py
+++ backend/server.py
@@ -1,17 +1,31 @@
 """
 This is the starting script for a flask server app
+page routes:
+    '/' index, return index.html
+    
+api routes:
+    '/api/wordcloud' return json, formatted wordcloud data structure
+
 """
 
 from flask import Flask, request, Response
 import json
 import os
 
-static_path = os.path.abspath(os.path.dirname(__file__))
-
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__,
             static_url_path='',
             static_folder='../frontend/build')
+
+
+@app.route('/api/wordcloud', methods=['get'])
+def getWordCloud():
+    """
+    generate a wordcloud json and return to client
+    """
+    new_wordcloud = {'hello': 10, 'yes': 9, 'foo': 8, 'bar': 7}
+    print(json.dumps(new_wordcloud))
+    return json.dumps(new_wordcloud)
 
 
 @app.route('/')
@@ -23,4 +37,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(port=3459)
+    app.run(port=3000)
