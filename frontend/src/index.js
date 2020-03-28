3ff5825666e418c61db0e28a124686141ce37dbe
Johnny Wan <wonjohnny@outlook.com>
2020-03-27 20:57:40 -0700

    testing out callbacks

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,16 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import WordCloud from "./wordcloud.js";
 
-// function Word(props) {
-//     let size_str = (5*props.weight).toString() + "px";
-//     let style_obj = {
-//         fontSize: size_str,
-//         padding: "0px",
-//     };
-//     console.log(size_str)
-//     return <button id="word_cloud_bubble" style={style_obj}>{props.text}</button>;
-// }
-
 // main frame of home page
 class Main extends React.Component {
   constructor(props) {
