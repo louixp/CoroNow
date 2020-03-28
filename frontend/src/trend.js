87f424020fe10d5bdf7832c09b388f95d8888249
Johnny Wan <wonjohnny@outlook.com>
2020-03-28 10:38:43 -0700

    change format: indent 2 (was 4)

--- frontend/src/trend.js
+++ frontend/src/trend.js
@@ -6,35 +6,37 @@ import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 
 class Trend extends React.Component {
-    // props: enable, text, handleClose
-    constructor({props}) {
-        super(props);
-        this.state = {
-            state: "init",
-        };
-    }
+  // props: enable, text, handleClose
+  constructor({ props }) {
+    super(props);
+    this.state = {
+      state: "init"
+    };
+  }
 
-    render() {
-        if (this.state.state === "init") {
-            // render an overlay
-            const new_trend_window = (
-                <Modal 
-                    show={this.props.enable} 
-                    onHide={this.props.handleClose} 
-                    animation={false}
-                    size="xl"
-                    centered
-                >
-                    <Modal.Header closeButton>
-                        <Modal.Title>Trend of "{this.props.text.toUpperCase()}" over Past 24 Hours</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>{this.props.text}</Modal.Body>
-                </Modal>
-            );
+  render() {
+    if (this.state.state === "init") {
+      // render an overlay
+      const new_trend_window = (
+        <Modal
+          show={this.props.enable}
+          onHide={this.props.handleClose}
+          animation={false}
+          size="xl"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>
+              Trend of "{this.props.text.toUpperCase()}" over Past 24 Hours
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{this.props.text}</Modal.Body>
+        </Modal>
+      );
 
-            return new_trend_window;
-        }
+      return new_trend_window;
     }
+  }
 }
 
 export default Trend;
--- frontend/src/wordcloud.js
+++ frontend/src/wordcloud.js
@@ -5,80 +5,87 @@ import "d3-transition";
 import { select } from "d3-selection";
 
 function wordCloudCallBack(callback_str) {
-    return function(word, event) {
-        const isActive = callback_str !== 'onWordMouseOut';
-        const element = event.target;
-        const text = select(element);
-        text
-          .transition()
-          // .attr('background', 'white')
-          .attr('text-decoration', isActive ? 'underline' : 'none');
-    };
+  return function(word, event) {
+    const isActive = callback_str !== "onWordMouseOut";
+    const element = event.target;
+    const text = select(element);
+    text
+      .transition()
+      // .attr('background', 'white')
+      .attr("text-decoration", isActive ? "underline" : "none");
+  };
 }
 
 const wordcloud_options = {
-    colors: ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b"],
-    enableTooltip: true,
-    deterministic: true,
-    fontFamily: "helvetica",
-    fontSizes: [10, 60],
-    fontStyle: "normal",
-    fontWeight: "normal",
-    padding: 1,
-    rotations: 2,
-    rotationAngles: [0, 90],
-    scale: "sqrt",
-    spiral: "archimedean",
-    transitionDuration: 500
+  colors: ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b"],
+  enableTooltip: true,
+  deterministic: true,
+  fontFamily: "helvetica",
+  fontSizes: [10, 60],
+  fontStyle: "normal",
+  fontWeight: "normal",
+  padding: 1,
+  rotations: 2,
+  rotationAngles: [0, 90],
+  scale: "sqrt",
+  spiral: "archimedean",
+  transitionDuration: 500
 };
 
 // wordcloud window, a div element, containing a bunch of words
 class WordCloud extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            state: "init",
-            words: props.words,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      state: "init",
+      words: props.words
+    };
+  }
 
-    shouldComponentUpdate(newProps, newStates) {
-        console.log("will update: " + (this.props.ver !== newProps.ver));
-        return (this.props.ver !== newProps.ver)
-    }
+  shouldComponentUpdate(newProps, newStates) {
+    console.log("will update: " + (this.props.ver !== newProps.ver));
+    return this.props.ver !== newProps.ver;
+  }
 
-    wordClickCallback(word) {
-        this.props.onWordClick(word);
-    }
+  wordClickCallback(word) {
+    this.props.onWordClick(word);
+  }
 
-    wordCloudCallbacks = {
-        // getWordColor: word => (word.value > 50 ? 'orange' : 'purple'),
-        getWordTooltip: word =>
-            `See the trend of "${word.text}" on social media`,
-        onWordClick: (word, event)=>this.wordClickCallback(word.text),
-        onWordMouseOut: wordCloudCallBack('onWordMouseOut'),
-        onWordMouseOver: wordCloudCallBack('onWordMouseOver'),
-    }
+  wordCloudCallbacks = {
+    // getWordColor: word => (word.value > 50 ? 'orange' : 'purple'),
+    getWordTooltip: word => `See the trend of "${word.text}" on social media`,
+    onWordClick: (word, event) => this.wordClickCallback(word.text),
+    onWordMouseOut: wordCloudCallBack("onWordMouseOut"),
+    onWordMouseOver: wordCloudCallBack("onWordMouseOver")
+  };
 
-    render() {
-        console.log("wordcloud render");
-        if (this.state.state === "init") {
-            return (
-                <div style={{ width: this.props.width, height: this.props.height }}>
-                    <div
-                        style={{ width: "100%", height: "100%" }}
-                        id="word-cloud-container"
-                    >
-                        <ReactWordcloud
-                            words={this.props.words}
-                            options={wordcloud_options}
-                            callbacks={this.wordCloudCallbacks}
-                        />
-                    </div>
-                </div>
-            );
-        }
+  render() {
+    console.log("wordcloud render");
+    if (this.state.state === "init") {
+      return (
+        <div
+          style={{
+            width: this.props.width,
+            height: this.props.height
+          }}
+        >
+          <div
+            style={{
+              width: "100%",
+              height: "100%"
+            }}
+            id="word-cloud-container"
+          >
+            <ReactWordcloud
+              words={this.props.words}
+              options={wordcloud_options}
+              callbacks={this.wordCloudCallbacks}
+            />{" "}
+          </div>{" "}
+        </div>
+      );
     }
+  }
 }
 
 export default WordCloud;
