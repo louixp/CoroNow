33f4267a83ee37e2cc949970d405d2420ff053a4
Johnny Wan <wonjohnny@outlook.com>
2020-03-29 09:39:50 -0700

    working implementation of sentiment and trend, styling still needed

--- frontend/src/sentiments.js
+++ frontend/src/sentiments.js
@@ -1,5 +1,8 @@
 import React from "react";
+import CanvasJSReact from "./lib/canvasjs.react";
 import "./style/sentiments.css";
+
+let CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 class Sentiments extends React.Component {
   constructor(props) {
@@ -15,21 +18,25 @@ class Sentiments extends React.Component {
       <div
         style={{
           height: "100%",
-          width: "100%"
+          width: "100%",
         }}
         id={this.props.id}
       >
-        <div>this is sentiment</div>
-        {categories.map((val, ind) => {
-          let sen = this.props.sentiment_list[val]; // sentiment array of the current category
-          return (
-            <SentimentItem
-              category={val}
-              sentiment_list={sen}
-              id={"sentiment_" + val}
-            />
-          );
-        })}
+        <div className="font-effect-3d" id="sentiment_title">
+          SENTIMENT ANALYSIS
+        </div>
+        <div id="sentiment_contents">
+          {categories.map((val, ind) => {
+            let sen = this.props.sentiment_list[val]; // sentiment array of the current category
+            return (
+              <SentimentItem
+                category={val}
+                sentiment_list={sen}
+                id={"sentiment_" + val}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
@@ -67,18 +74,72 @@ class SentimentItem extends React.Component {
     if (this.state.state === "show_last") {
       // display a single line
       return (
-        <div id={this.props.id} onClick={() => this.expand()}>
+        <div id={this.props.id} onClick={() => this.expand()} className="sentiment_small">
           {cat}: {sen[sen.length - 1].value}
         </div>
       );
     } else if (this.state.state === "show_trend") {
       // display trend of this category
       return (
-        <div id={this.props.id} onClick={() => this.collapse()}>
-          this item has been expanded, click to collapse
+        <div id={this.props.id} onClick={() => this.collapse()} className="sentiment_big">
+          <SentimentTrend data={sen} category={this.props.category}/>
         </div>
       );
     }
+  }
+}
+
+class SentimentTrend extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    let data = this.props.data;
+    let trend_graph = null;
+    if (data != []) {
+      let sen_arr = [];
+      for (let i = 0; i < data.length; i++) {
+        let sen = data[i];
+        console.log(sen);
+        let sen_date = sen.timestamp.split("-");
+        sen_arr.push({
+          x: new Date(
+            parseInt(sen_date[0], 10),
+            parseInt(sen_date[1], 10) - 1,
+            parseInt(sen_date[2], 10),
+            parseInt(sen_date[3], 10)
+          ),
+          y: sen.value
+        });
+      }
+      const options = {
+        animationEnabled: true,
+        title: {
+          text: "Sentiment trend about " + this.props.category
+        },
+        backgroundColor: "#eeeeee",
+        axisX: {
+          valueFormatString: "MMM.DD HH:00",
+          lineThickness: 4
+        },
+        axisY: {
+          title: "Frequency",
+          includeZero: true,
+          maximum: 1,
+          minimum: -1
+        },
+        data: [
+          {
+            type: "line",
+            dataPoints: sen_arr
+          }
+        ]
+      };
+      trend_graph = <CanvasJSChart options={options} />;
+    }
+    return (
+      <div className="sentiment_trend">{trend_graph}</div>
+    );
   }
 }
 
