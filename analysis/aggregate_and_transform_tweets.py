e63a52a8c2dc56430f0e020fdb9cbce6dae2cf90
William Zhu <45189159+WilliamYZhu@users.noreply.github.com>
2020-03-28 20:28:10 -0700

    Create aggregate_and_transform_tweets.py

---
+++ analysis/aggregate_and_transform_tweets.py
@@ -0,0 +1,14 @@
+import string_arr_to_XML
+
+#all tweets within an entire timestamp directory on FireBase (like 2020-03-26-13) => single xml file 
+def save_xml(firebase, time):
+    end_time[-1] = end_time[-1] + 30
+    keywords = []
+    for 
+
+
+    firebase.retrieve_data("tweets", , f)
+
+    xml = convert_to_XML(firebase, start_time=[], end_time=end_time, keywords=keywords, entry="tweet"):
+
+
